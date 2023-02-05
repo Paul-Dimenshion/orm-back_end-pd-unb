c836cbecdb566ab67e19f119143909007f212178
PAVLO DEMENSHYN <89635409+Paul-Dimenshion@users.noreply.github.com>
2023-02-04 23:30:51 -0400

    find a single tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -19,6 +19,18 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  try {
+    const singleTagData = Tag.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+    if (!singleTagData) {
+      res.status(404).json({ message: 'No tag was found with that id!' });
+      return;
+    }
+    res.status(200).json(singleTagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
