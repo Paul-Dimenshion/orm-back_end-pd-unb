dcb1966cba64f357d4adbda152ec731e97bb867e
PAVLO DEMENSHYN <89635409+Paul-Dimenshion@users.noreply.github.com>
2023-02-04 22:18:17 -0400

    category search

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,6 +19,19 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const singleCategoryData = Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+
+    if (!singleCategoryData) {
+      res.status(404).json({ message: 'No Category was found with that id!' });
+      return;
+    }
+    res.status(200).json(singleCategoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
