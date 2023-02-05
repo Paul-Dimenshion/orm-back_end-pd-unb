a5ff9f37714ab8d4ed7bf73775c9323ac1315dc9
PAVLO DEMENSHYN <89635409+Paul-Dimenshion@users.noreply.github.com>
2023-02-04 22:59:40 -0400

    find one product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,6 +21,20 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const singleProductData = Product.findByPk(req.params.id, {
+      include: [{ model: Category },{ model: Tag }]
+    });
+
+    if (!singleProductData) {
+      res.status(404).json({ message: 'No Product was found with that id!' });
+      return;
+    }
+
+    res.status(200).json(singleProductData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
